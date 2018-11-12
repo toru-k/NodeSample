fe318e2b12c7b18270add08f8a1c3c2117c783fe
toru-k <toru_kobayashi@three-hands-works.com>
2018-11-12 21:02:50 +0900

    Server 機能を外部ファイル化

--- app.ts
+++ app.ts
@@ -1,19 +1,8 @@
-import * as http from "http";
-
+import { ServerAPI } from "./serverModule";
 class Main {
   constructor() {
-    const server: http.Server = http.createServer(
-      (request: http.IncomingMessage, response: http.ServerResponse) =>
-        this.requestHandler(request, response)
-    );
-    server.listen("5000");
-  }
-
-  private requestHandler(
-    request: http.IncomingMessage,
-    response: http.ServerResponse
-  ): void {
-    response.end("Hello! Node.js with TypeScript");
+    const serverAPI = new ServerAPI();
+    serverAPI.initServer();
   }
 }
 
