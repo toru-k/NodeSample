643cfbca1a0fc7d621e27652dc21bfa96ca1ebce
unknown <z2j7462@intra.ctc-g.co.jp>
2018-11-12 16:46:55 +0900

    サーバー実装

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log("Hello! Node.js × TypeScript");
+import * as http from "http";
+
+class Main {
+  constructor() {
+    const server: http.Server = http.createServer(
+      (request: http.IncomingMessage, response: http.ServerResponse) =>
+        this.requestHandler(request, response)
+    );
+    server.listen("5000");
+  }
+
+  private requestHandler(
+    request: http.IncomingMessage,
+    response: http.ServerResponse
+  ): void {
+    response.end("Hello! Node.js with TypeScript");
+  }
+}
+
+const main = new Main();
